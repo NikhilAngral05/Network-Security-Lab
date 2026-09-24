99f1532afc12186def60378e5f09872324b05cea
Nikhil Angral <2024a7r023@mietjammu.in>
2026-09-24 10:48:48 +0530

    Update challenge_response.py

--- Experiment-3-Challenge-Response-Authentication/codes/challenge_response.py
+++ Experiment-3-Challenge-Response-Authentication/codes/challenge_response.py
@@ -1,1 +1,48 @@
+import hashlib
+import secrets
 
+# Shared secret
+shared_key = "mysecret"
+
+# Set to store previously used challenges
+used_challenges = set()
+
+# Create a random challenge
+server_challenge = secrets.token_hex(8)
+
+print("Server Challenge:", server_challenge)
+
+# Client creates authentication response
+client_response = hashlib.sha256(
+    (server_challenge + shared_key).encode()
+).hexdigest()
+
+print("Client Response:", client_response)
+
+# Server creates expected response
+server_response = hashlib.sha256(
+    (server_challenge + shared_key).encode()
+).hexdigest()
+
+# Verify authentication
+if client_response == server_response:
+    print("Authentication Successful")
+
+    # Check if challenge was already used
+    if server_challenge in used_challenges:
+        print("Replay Attack Detected")
+    else:
+        used_challenges.add(server_challenge)
+        print("Challenge accepted.")
+
+else:
+    print("Authentication Failed")
+
+# Replay attack test
+print("\nReplay Attack Simulation")
+
+if server_challenge in used_challenges:
+    print("Replay Attack Detected")
+else:
+    used_challenges.add(server_challenge)
+    print("Response Accepted")
